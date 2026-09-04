9d01e031f16b9f0608ff4fbc6e0e2c3cfa87d2eb
Sahariar Islam <sahariarislam119@gmail.com>
2026-09-04 23:37:58 +0530

    Bubble Sort.

---
+++ src/Basic_Sorting/BubbleSort.java
@@ -0,0 +1,41 @@
+package Basic_Sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static int[] arrayInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static void printArr(int[]arr){
+        for(int ele: arr){
+            System.out.print(ele+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = arrayInput();
+        for(int i=0;i<arr.length-1;i++){
+            boolean isSorted = true;
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    isSorted = false;
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+            if(isSorted) break;
+        }
+        System.out.print("Sorted array: ");
+        printArr(arr);
+    }
+}
---
+++ src/Basic_Sorting/CheckSortedOrNot.java
@@ -0,0 +1,33 @@
+package Basic_Sorting;
+
+import java.util.Scanner;
+
+public class CheckSortedOrNot {
+
+    public static int[] arrayInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static void checking(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                System.out.println("Array is not Sorted.");
+                return;
+            }
+        }
+        System.out.println("Array is sorted.");
+    }
+
+    public static void main(String[] args) {
+        int[] arr = arrayInput();
+        checking(arr);
+    }
+}
