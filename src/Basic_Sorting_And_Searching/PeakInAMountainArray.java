23f4e5d92ecfb0ac5f5411a4a1675a6185abf3c4
Sahariar Islam <sahariarislam119@gmail.com>
2026-09-10 23:50:01 +0530

    Searching in Decreasing Array and Peak Element in Mountain Array

---
+++ src/Basic_Sorting_And_Searching/PeakInAMountainArray.java
@@ -0,0 +1,34 @@
+package Basic_Sorting_And_Searching;
+
+import java.util.Scanner;
+
+public class PeakInAMountainArray {
+    public static int[] arrayInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter mountain array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = arrayInput();
+        int high=arr.length-1,low=0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                System.out.println("The Peak index: "+mid);
+                System.out.println("The Peak element: "+arr[mid]);
+                return;
+            }
+//            Increasing part
+            else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]) low=mid+1;
+//            Decreasing Part
+            else if(arr[mid]<arr[mid-1] && arr[mid]>arr[mid+1]) high=mid-1;
+        }
+    }
+}
