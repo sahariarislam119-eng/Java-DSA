975ae60e821c0a3eee729810c88c82c7951df681
Sahariar Islam <sahariarislam119@gmail.com>
2026-09-19 23:33:10 +0530

    String is Palindrome or Not

--- src/Strings/StringIsPalindromeOrNot.java
+++ src/Strings/StringIsPalindromeOrNot.java
@@ -1,4 +1,20 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class StringIsPalindromeOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int i=0,j=str.length()-1;
+        while(i<=j){
+            if(str.charAt(i)!=str.charAt(j)){
+                System.out.println("String is not Palindrome.");
+                return;
+            }
+            i++;
+            j--;
+        }
+        System.out.println("String is Palindrome.");
+    }
 }
